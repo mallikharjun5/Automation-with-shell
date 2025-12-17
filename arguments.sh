264ec71ee72b7557c15d32b6fc158203bf632474
mallikharjun <mallikharjunandey5@gmail.com>
2025-12-17 10:58:25 +0530

    arguments

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person2:: hi $person1 how are you"
+echo "$person1:: i am fine $person2"
+echo "$person2:: ok, what you are learing present"
+echo "$person1:: currently i am learning shell scripting $person2"
