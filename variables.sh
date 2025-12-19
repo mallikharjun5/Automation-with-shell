1b78a3acfd621ea5550b45deeab314e8bec8ea4a
mallikharjun <mallikharjunandey5@gmail.com>
2025-12-19 12:17:37 +0530

    dec19

--- variables.sh
+++ variables.sh
@@ -5,3 +5,5 @@ person2=arjun
 
 echo "$person2:: hi $person1 how are you"
 echo "$person1:: i am fine $person2"
+echo "$person2:: ok, what you are learning now"
+echo "$person1:: at present i am learning AWS"
