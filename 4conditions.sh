1b711fb5e1141376a08e1c829c03e2e147b268e3
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-04 12:14:44 +0530

    jan4 4conditions

--- 4conditions.sh
+++ 4conditions.sh
@@ -3,7 +3,7 @@
 echo "please enter the number"
 read -s NUMBER
 
-if [ $(($NUMBER % 1)) != 0 ]; then
+if [ $(($NUMBER % 2)) -le 1 ]; then
     echo "given number $NUMBER is prime"
 else
     echo "given number $NUMBER is not prime"
