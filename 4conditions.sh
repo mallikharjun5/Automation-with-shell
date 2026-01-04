3a204cd15241067809289cf7e39762019294db5d
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-04 12:17:48 +0530

    jan4 4conditions

--- 4conditions.sh
+++ 4conditions.sh
@@ -3,8 +3,8 @@
 echo "please enter the number"
 read -s NUMBER
 
-if [ $(($NUMBER % 2)) -le 1 ]; then
-    echo "given number $NUMBER is prime"
-else
+if [ $NUMBER -le 1 ]; then
     echo "given number $NUMBER is not prime"
+else
+    echo "given number $NUMBER is prime"
 fi
