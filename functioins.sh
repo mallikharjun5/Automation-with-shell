fe70bccd7e4dcd8b171c25394b489175570400fb
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-05 12:12:24 +0530

    functions

---
+++ functioins.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Error:: please run this script with root privilege"
+    exit 1    # failure is other than 0
+fi
+
+
+VALIDATE (){
+    if [ $1 -ne 0 ]; then
+        echo "Error:: Installing $2 is failure"
+        exit 1
+    else
+        echo "Installing $2 is success"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install mongodb-mongosh -y
+VALIDATE $? "mongosh"
