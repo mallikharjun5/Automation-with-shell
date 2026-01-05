ae5a843d416d2f7cc8fa963af7e163f0a3afcf62
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-05 19:19:51 +0530

    colors

---
+++ colors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo -e "$R Hi mallikharjun $N"
+
+echo -e "$G How are you $N"
+
+echo -e "$Y Hope you are doing fine $N"
+
+echo -e "$N See you seen $N"
