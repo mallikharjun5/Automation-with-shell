f6ab8cd1b327481931f71ec9a50cc23667c23edb
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-09 20:04:24 +0530

    new

--- sample.sh
+++ sample.sh
@@ -3,6 +3,7 @@
 echo "Hi i am shell, i can automate tasks"
 if [ $? -ne 0 ] then
     echo "first command was successfull"
+    exit 1
 else
     echo "check the syntax and try again... $Y....$N"
 fi
