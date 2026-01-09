9179b2b538055363923e9852ded30d5154e5c715
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-09 19:59:16 +0530

    new

---
+++ arg_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1="$1"
+person2="$2"
+
+echo "$person2:: hi $person1 how are you"
+echo "$person1:: i am fine $person2"
+echo "$person2:: ok, what you are learning now"
+echo "$person1:: at present i am learning AWS"
