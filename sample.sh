cedac35c83bfe7ed913eb716c5d39e247d9385b3
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-10 10:15:17 +0530

    new

--- sample.sh
+++ sample.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+set -euo pipefail
+
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+
+
 echo "Hi i am shell, i can automate tasks"
 if [ $? -ne 1 ]; then
     echo "first command was successfull"
 else
     echo "check the syntax and try again..."
 fi
+
+kdahbka;ashvd
