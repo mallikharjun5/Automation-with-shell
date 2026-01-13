e4bb26872ec55da8dacda9cd673ae5ca23db4016
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-13 16:07:46 +0530

    while

--- while.sh
+++ while.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+count=5
+
+echo "Starting Countdown/..."
+
+while [ $count -gt 0 ]
+do
+    echo "Time left: $count"
+    sleep 1 #pause for one second
+    count=$((count - 1)) # Decrement the count
+done
+
+echo "Times up"
