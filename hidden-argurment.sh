3543a6d35c9bf7e0b0be296c0f4f32d2aa3dac9e
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-25 17:20:35 +0530

    modified

--- hidden-argurment.sh
+++ hidden-argurment.sh
@@ -2,6 +2,6 @@
 
 echo "enter your pin number"
 
-read -s PIN_NUMBER # here pin number is variable
+read -s PIN_NUMBER # here pin number is variable and read -s helps to hide arguments when we passing in CLI
 
 echo "pin entered is: $PIN_NUMBER"
