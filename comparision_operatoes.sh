97b2fd5358a70838fe2b1c1f6bfe8a0b17231d66
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-28 11:15:13 +0530

    comparision operators

---
+++ comparision_operatoes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+a=20
+b=30
+
+echo "comparision operations"
+
+if [ $a -eq $b ]; then
+    echo "a is equal to b"
+else
+    echo "a is not equal to b"
+fi
+
+if [ $a -ne $b ]; then
+    echo "a is not equal to b"
+else
+    echo "a is equal to b"
+fi
+
+if [ $a -gt $b ]; then
+    echo "a is greater than b"
+else
+    echo "a is less than b"
+fi
+
+if [ $a -lt $b ]; then
+    echo "a is less than b"
+else
+    echo "a is greater than b"
+fi
