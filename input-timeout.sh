d2a62a0ff31f3f793c30eaa302005d662165a33f
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-28 10:12:58 +0530

    timeout input

--- input-timeout.sh
+++ input-timeout.sh
@@ -2,7 +2,7 @@
 
 read -t 10 -p "enter your name with in 10 seconds :" name
 
-if [ -z $name]; then
+if [ -z $name ]; then
     echo "no name entered in 10 seconds"
 else
     echo "your name is: $name "
