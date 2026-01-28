2af18a134b18fc763f35daf8d70d6c460cf2d9b9
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-28 10:44:40 +0530

    operators

--- operators.sh
+++ operators.sh
@@ -5,3 +5,7 @@ read -s b
 echo "A=$a, B=$b"
 echo "Arithmetic operation of A and B"
 echo "addition of two numbers $(( a + b ))"
+echo "subtraction of two numbers $(( a - b ))"
+echo "multiplication of two numbers $(( a * b ))"
+echo "division of two numbers $(( a / b ))"
+echo "modulus of two numbers $(( a % b ))"
