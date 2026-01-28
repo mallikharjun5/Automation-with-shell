3de89a70ed9f08b3ec6928fce1bd7233173af39d
mallikharjun <mallikharjunandey5@gmail.com>
2026-01-28 12:12:11 +0530

    string operators

---
+++ string_operators.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+str1=hello
+str2=world
+
+echo "string operational"
+echo "=================="
+
+if [ "$str1 = $str2" ]; then
+    echo "strings are equal"
+fi
+
+if [ "$str1 != $str2" ]; then
+    echo "strings are not equal"
+fi
+
+str3= "$str1 $str2"
+echo "Concatenated string : $str3"
+length= ${#str3}
+echo "length of str3 is $length"
