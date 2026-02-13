6486fc7580ba777d1ffc05f891dbf1285cdfe1c4
mallikharjun <mallikharjunandey5@gmail.com>
2026-02-13 09:24:58 +0530

    var

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PWD=$(/home)
+
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "Script name is: $0"
 echo "Script running by: $USER"
-echo "Home directory of the user is: $USER"
+echo "Home directory of the user is: $PWD"
 echo "PID of the script is: $$"
 sleep 30 &
 echo "PID of the last command in background is: $!"
